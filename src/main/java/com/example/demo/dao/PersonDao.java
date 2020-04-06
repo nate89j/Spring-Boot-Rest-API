c42af55f65e50692ca261b48caa3798b97064c22
nat leo <anastasia.leontis@gmail.com>
2020-04-06 23:07:51 +0300

    dao working with postman

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ *
+ * @author Nat;
+ */
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonBYId(UUID id, Person person);
+
+}
